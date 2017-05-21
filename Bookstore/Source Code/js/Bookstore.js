f9b23d9fb6cb00664468af06d337b6a64b03f6b9
Gustavo Ardito <gardito@eventbrite.com>
2017-05-21 13:39:58 -0300

    1st step book

---
+++ Bookstore/Source Code/js/Bookstore.js
@@ -0,0 +1,27 @@
+var bookStoreApp = new Backbone.Marionette.Application();
+var BookStoreController = Backbone.Marionette.Controller.extend({
+     displayBooks : function (){
+       console.log("I will display books...");
+     }
+});
+var BookStoreRouter = Backbone.Marionette.AppRouter.extend({
+     controller :  BookStoreController,
+     appRoutes: {
+         "": "displayBooks"
+  }
+});
+
+BookStoreApp.addInitializer(function () {
+    var bookStoreController = new BookStoreController();
+    var bookStoreRouter = new BookStoreRouter({controller:controller});
+    console.log('Message from the addInitializer Method');
+});
+
+BookStoreApp.on('initialize:after', function () {
+    if (Backbone.history) {
+        Backbone.history.start();
+    }
+    console.log('Mesagge from initialize:after method');
+});
+
+BookStoreApp.start();
